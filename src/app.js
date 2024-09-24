555c99c9041794253db3907c332481a378788be8
Jm9710 <85179035+Jm9710@users.noreply.github.com>
2024-09-24 00:40:02 +0000

    Generador de cartas finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,55 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let bakcgroundCard = document.querySelector("#background-card");
+  bakcgroundCard.style.backgroundColor = "green";
+
+  const generateRandom = () => {
+    const number = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12"
+    ];
+
+    const suit = ["♦", "♥", "♠", "♣"];
+
+    const suitColors = {
+      "♦": "red",
+      "♥": "red",
+      "♠": "black",
+      "♣": "black"
+    };
+
+    function cardRandom(arr) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    }
+
+    const randomNumber = cardRandom(number);
+    const randomSuit = cardRandom(suit);
+
+    document.getElementById("cardNumber").innerHTML = randomNumber;
+
+    const suitElements = document.querySelectorAll(".cardSuite");
+    suitElements.forEach(elem => {
+      elem.innerHTML = randomSuit;
+      elem.style.color = suitColors[randomSuit];
+    });
+  };
+
+  let reload = document.getElementById("refresh");
+
+  reload.addEventListener("click", () => {
+    location.reload();
+  });
+
+  generateRandom();
 };
